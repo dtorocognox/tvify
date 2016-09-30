75fad7a7d19838638efca3973bd747c679fd3ae7
David Toro Alzate <dtoro@cognox.com>
2016-09-30 12:01:49 -0500

    modificaciones

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
 $(function () {
-    var header = document.getElementById("app-header");
-    var selection = $([document, header]);
 
-    $('header[data-title="TVify"]')
+    //Submit search form
+
+
+
+    $('#app-body')
+        .find('form')
+        .submit(function onsubmit (ev){
+            ev.preventDefault();
+            var busqueda = $(this)
+                .find('input[type="text"]')
+                .val();
+            alert("Se ha buscado: " + busqueda);
+        })
+
+    var template = '<article class="tv-show">' +
+        '<div class="left img-container">' +
+        '<img src=":img:" alt=":img alt:"/>' +
+        '</div> ' +
+        '<div class="right info"> ' +
+        '<h1>:name:</h1>' +
+        '<p>:summary:</p>' +
+        '</div>' +
+        '</article>';
+
+    $.ajax(
+        {url: 'http://api.tvmaze.com/shows',
+        success: function (show, textStatus, xhr){
+            var $tvShowsContainer = $('#app-body').find('.tv-shows');
+            show.forEach(function (show){
+                var article = template
+                    .replace(':name:', show.name)
+                    .replace(':img:', show.image.medium)
+                    .replace(':summary:', show.summary)
+                    .replace(':img alt:', show.name + "Logo")
+                    $tvShowsContainer.append($(article));
+            })
+        }
+    })
 })
---
+++ prueba.js
@@ -0,0 +1,6 @@
+$('input').on("click", function () {
+        alert("o click o cambio");
+    }
+).on("click", function(){
+    console.log("segunda función");
+});
